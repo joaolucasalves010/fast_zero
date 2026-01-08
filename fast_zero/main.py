5564e843088e990d0504a7c83ce94dea5b77ea25
joaolucasalves010 <ylucasslalves@gmail.com>
2026-01-08 14:25:40 -0300

    corpo da requisição post/put

--- fast_zero/main.py
+++ fast_zero/main.py
@@ -62,6 +62,7 @@ async def read_file(file_path: str):
     return {"file_path": file_path}
 
 
+# Parâmetros de consulta
 fake_books_db = [{"book_id": 1, "book_name": "Harry Potter"}, {"book_id": 2, "book_name": "Ice And Fire"}, {"book_id": 3, "book_name": "Grokking Algorithms"}]
 
 @app.get("/books/")
@@ -79,3 +80,28 @@ async def read_book(book_id: int | None = None, book_name: str | None = None, q:
                 return {"book_id": book['book_id'], "book_name": book['book_name'], "description": f"{book['book_name']} is a nice book, have a long description"}
             
     return "Book not found"
+
+# Corpo da requisição
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/products/")
+async def create_product(product: Product):
+    product_dict = product.model_dump()
+    if product.tax is not None:
+        price_with_tax = product.price + product.tax
+        product_dict['price_with_tax'] = product.price + product.tax
+    return product_dict
+
+@app.put("/producrs/{product_id}")
+async def update_product(product_id: int, product: Product, q: str | None = None):
+    product_dict = product.model_dump()
+    result = {"product_id": product_id, **product_dict}
+    if q:
+        result['q'] = q
+    return result

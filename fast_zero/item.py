2e265706018c1f1c7d8ab9c4094712a0d56db812
João Lucas <ylucasslalves@gmail.com>
2026-01-13 11:03:11 -0300

    valores de examplo da documentação com json_schema e examples no Field

--- fast_zero/item.py
+++ fast_zero/item.py
@@ -5,8 +5,34 @@ class Item(BaseModel):
     name: str
     description: str | None = Field(default=None, title="The description of the item", max_length=300)
     price: float = Field(gt=0, title="The price must be greater than zero")
-    tax: float | None = None
+    tax: float | None = Field(default=None, examples=[3.2])
     # tags: list[str] = [] -> lista que aceita somente strings
     tags: set[str] = set() # -> por ser um campo tags, não pode ter valor repetido então definimos um conjunto que aceita somente tring
     # image: Image | None = None -> Modelo aninhado
     images: list[Image] | None = None # -> BaseModel como subtipo
+
+    """
+    
+    model_config = { -> muda o json_schema de exemplo na documentação da API
+        "json_schema_extra": {
+            "example": {
+                "item": {
+                    "name": "Product 1",
+                    "description": "Product 1 description",
+                    "price": 90,
+                    "tax": 30,
+                    "tags": ["product", "Product_1"],
+                    "images": [
+                        {
+                            "name": "Product 1",
+                            "url": "http://localhost:8000/images/product1.png"
+                        }
+                    ]
+                },
+            }
+        }
+    }
+    
+    """
+
+

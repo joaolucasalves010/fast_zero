c5abcc2298db2b351b6c0a3d9bb2d0128829c65d
João Lucas <ylucasslalves@gmail.com>
2026-01-28 23:42:02 -0300

    dependencies como classes

--- fast_zero/dependencies.py
+++ fast_zero/dependencies.py
@@ -9,11 +9,26 @@ async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 10
 
 CommonsDep = Annotated[dict, Depends(common_parameters)]
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# Dependencias como Classes
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
 @app.get("/items/")
-async def read_items(commons: CommonsDep, tag: str = None):
+async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)], tag: str = None):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
     if tag:
-        commons["tag"] = tag
-    return commons
+        response.update({"tag": tag})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
 
 @app.get("/users/")
 async def read_users(commons: Annotated[dict, Depends(common_parameters)], username: str):

615d01dac17849fae4daf37f6bd6c70cb9832efa
João Lucas <ylucasslalves@gmail.com>
2026-01-31 20:13:13 -0300

    Simples OAuth2 com senha e Bearer¶

--- fast_zero_security/main.py
+++ fast_zero_security/main.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from typing import Annotated
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from pydantic import BaseModel, EmailStr
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
 app = FastAPI()
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") # Declara que o endpoint com /token será onde o usuário consiguira seu token Bearer
 
@@ -14,24 +34,56 @@ class User(BaseModel):
     full_name: str | None = None
     disabled: bool | None = None
 
+class UserInDb(User):
+    hashed_password: str
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDb(**user_dict)
+
 def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
+    user = get_user(fake_users_db, token)
+    return user
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect usename or password")
+    user = UserInDb(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect usename or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
 @app.get("/users/me")
-async def read_user_me(current_user: Annotated[User, Depends(get_current_user)]):
+async def read_user_me(current_user: Annotated[User, Depends(get_current_active_user)]):
     return current_user
 
 
 @app.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]): # Depende do Bearer token no Header da requisição para funcionar
     return {"token": token}
-
 
 """
 
@@ -46,6 +98,5 @@ Backend autentica o usuário
 ↓
 
 Retorna access_token (Bearer)
-
 
 """
